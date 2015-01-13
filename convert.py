65cd6e50face8a0cdf5b768989eb9cde78f8c621
crberry <crberry@uchicago.edu>
2015-01-13 10:25:36 -0600

    On laptop
    folder holding code on laptop

---
+++ convert.py
@@ -0,0 +1,11 @@
+#convert.py
+# A program to convert Censius temperature to Farenheit
+# by Christopher Berry
+
+def main():
+    celsius = eval(input("What is the Celsius temperature? "))
+    farenheit = 9/5 * celsius + 32
+    print("The temperature is",farenheit,"degrees Farenheit.")
+
+main()
+    
